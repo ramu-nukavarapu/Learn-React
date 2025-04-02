cac4068f93091ba6ed8a1cff062bf16eff072a6a
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-04-02 22:29:30 +0530

    Update 'App.jsx' file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Greeting, GreetUser, Sum, UserLogin } from './learnings/01 Components'
+import { Incrementer } from './learnings/02 States'
+import { GreetUserWithName } from './learnings/03 Props'
+import { UserDetails, UserDetailsValidation, UserDetailsValidationByProps } from './learnings/04 Array Rendering'
+import { IncrementByOne } from './learnings/05 UseEffect Hook'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Incrementer /> */}
+      <Greeting />
+      <GreetUser />
+      <Sum />
+      <UserLogin />
+      <GreetUserWithName name={"Ramu Nukavarapu"}/>
+      <UserDetails />
+      <UserDetailsValidation />
+      <UserDetailsValidationByProps />
+      <IncrementByOne />
     </>
   )
 }
