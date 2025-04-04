4271650911502d7b4fe31a81df8f5aed28b7e1c3
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-04-04 23:35:04 +0530

    Update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import { Incrementer } from './learnings/02 States'
 import { GreetUserWithName } from './learnings/03 Props'
 import { UserDetails, UserDetailsValidation, UserDetailsValidationByProps } from './learnings/04 Array Rendering'
 import { IncrementByOne } from './learnings/05 UseEffect Hook'
+import { Signin, Signup } from './learnings/06 Routing'
+import { Routing } from './learnings/06 Routing'
 
 function App() {
 
   return (
     <>
+      <Routing />     
       {/* <Incrementer /> */}
       <Greeting />
       <GreetUser />
@@ -18,7 +21,8 @@ function App() {
       <UserDetails />
       <UserDetailsValidation />
       <UserDetailsValidationByProps />
-      <IncrementByOne />
+      {/* <IncrementByOne /> */}
+      
     </>
   )
 }
